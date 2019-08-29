901e9991d572253c6fd1dc842b08a5b40f57fa74
KIMBtech <info@kimb-technologies.eu>
2019-08-29 15:32:14 +0200

    Update mail.php

--- php/mail.php
+++ php/mail.php
@@ -2,5 +2,5 @@
 /**
  * Mail Test
  */
-mail( 'test@kimb-technologies.eu', 'Test Mail from Docker Image', 'Test ' . date( 'H:i:s d.m.Y' ) ); //, 'From: aaa@5d7.eu' );
+mail( 'test@kimb-technologies.eu', 'Test Mail from Docker Image', 'Test ' . date( 'H:i:s d.m.Y' ) );
 ?>
